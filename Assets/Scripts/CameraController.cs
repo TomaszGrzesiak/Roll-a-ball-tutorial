f53b81869751ada39dfeb1d489e8bf428bf91796
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-02-24 19:41:36 +0100

    feat(camera): Exercise 5: camera following player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,20 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
     void Start()
     {
+        // the line below answer 50% of exercise 5
         offset = transform.position - player.transform.position;
+        print($"offset: {offset}, transform.position: {transform.position} - player: {player.transform.position}");
     }
 
     
     void LateUpdate()
     {
+        // the line below answer another 50% of the exercise 5
         transform.position = player.transform.position + offset;
+        print($"offset: {offset}, transform.position: {transform.position} + player: {player.transform.position}");
     }
 }
